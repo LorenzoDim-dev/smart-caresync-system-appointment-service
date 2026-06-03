63187631d27335c3aa2fd61536c1c05204558374
LorenzoDiMassimo <lorenzo.dimassimo@aesys.tech>
2026-06-03 10:15:34 +0200

    first commit

---
+++ src/test/java/com/smart_caresync_system/appointment_service/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_caresync_system.appointment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
